fc09f834ea5f9b5bc66d5b378356cec40fcc49cd
unknown <roghibbrr@gmail.com>
2019-10-21 12:41:35 +0700

    Initial commit

---
+++ app/src/main/java/com/yogiapp/listview/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.yogiapp.listview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.R.layout.simple_list_item_1
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //data yang akan ditampilkan ke dalam list view
+        val languages = listOf("Kotlin", "Phyton", "Ruby", "JavaScript", "PHP")
+
+        //memberikan adapter ke list view
+        list_view.adapter = ArrayAdapter(this, simple_list_item_1, languages)
+
+        //menampilkan item saat diklik
+        list_view.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this, "Anda memilih : ${languages[position]}",Toast.LENGTH_SHORT).show() }
+    }
+}
+
+
